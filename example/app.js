65e7dc99e9bcdd26446cabf72783f27ae781a2ff
karissa <krmckelv@gmail.com>
2016-07-08 18:48:15 -0700

    create composable ui component

--- example/app.js
+++ example/app.js
@@ -1,113 +1,17 @@
+var Stats = require('../')
 var ess = require('event-source-stream')
-var speedometer = require('speedometer')
-var prettyBytes = require('pretty-bytes')
 
-var $ = document.querySelector.bind(document)
-var $$ = function (name, sel) {
-  return $('#' + (name || 'unknown') + ' ' + sel)
-}
-
-var feeds = {}
-
-function get (name) {
-  if (!name) name = 'unknown'
-  var st = feeds[name]
-  if (!st) {
-    var div = document.createElement('div')
-    div.id = name
-    div.innerHTML = $('#template').innerHTML
-    div.className = 'feed'
-
-    if (name !== 'unknown') {
-      div.querySelector('.name').innerText = name
-    }
-
-    document.body.appendChild(div)
-
-    st = feeds[name] = {
-      blocks: 0,
-      uploadSpeed: speedometer(),
-      downloadSpeed: speedometer(),
-      div: div
-    }
-    console.log('new feed', st)
-  }
-
-  return st
-}
-
-setInterval(function () {
-  var keys = Object.keys(feeds)
-  for (var i = 0; i < keys.length; i++) {
-    var st = feeds[keys[i]]
-    $$(keys[i], '.upload-speed').innerText = prettyBytes(st.uploadSpeed()) + '/s'
-    $$(keys[i], '.download-speed').innerText = prettyBytes(st.downloadSpeed()) + '/s'
-  }
-}, 500)
+var stats = Stats(document.body)
 
 ess('http://' + window.location.host + '/events')
   .on('data', function (data) {
     data = JSON.parse(data)
     switch (data.type) {
-      case 'key': return onkey(data)
-      case 'peer-update': return onpeerupdate(data)
-      case 'feed': return onfeed(data)
-      case 'update': return onupdate(data)
-      case 'download': return ondownload(data)
-      case 'upload': return onupload(data)
+      case 'key': return stats.onkey(data)
+      case 'peer-update': return stats.onpeerupdate(data)
+      case 'feed': return stats.onfeed(data)
+      case 'update': return stats.onupdate(data)
+      case 'download': return stats.ondownload(data)
+      case 'upload': return stats.onupload(data)
     }
   })
-
-function appendDot (name, i) {
-  var div = document.createElement('div')
-  div.className = 'dot block-' + i
-  $$(name, '.blocks').appendChild(div)
-}
-
-function onkey (data) {
-  $('#key').innerText = data.key
-
-  while ($('.feed')) document.body.removeChild($('.feed'))
-  feeds = {}
-}
-
-function updateHeader (data) {
-  $$(data.name, '.overview').innerText = data.blocks.length + ' blocks (' + prettyBytes(data.bytes) + ')'
-}
-
-function onpeerupdate (data) {
-  $('#peers').innerText = 'Connected to ' + data.peers + ' peer' + (data.peers === 1 ? '' : 's')
-}
-
-function ondownload (data) {
-  get(data.name).downloadSpeed(data.bytes)
-  $$(data.name, '.block-' + data.index).style.backgroundColor = 'gray'
-}
-
-function onupload (data) {
-  get(data.name).uploadSpeed(data.bytes)
-  $$(data.name, '.block-' + data.index).style.backgroundColor = '#35b44f'
-  setTimeout(function () {
-    $$(data.name, '.block-' + data.index).style.backgroundColor = 'gray'
-  }, 500)
-}
-
-function onfeed (data) {
-  var blocks = get(data.name).blocks = data.blocks.length
-  updateHeader(data)
-
-  for (var i = 0; i < blocks; i++) {
-    appendDot(data.name, i)
-    if (data.blocks[i]) $$(data.name, '.block-' + i).style.backgroundColor = 'gray'
-  }
-}
-
-function onupdate (data) {
-  updateHeader(data)
-
-  for (var i = get(data.name).blocks; i < data.blocks.length; i++) {
-    get(data.name).blocks++
-    appendDot(data.name, i)
-    if (data.blocks[i]) $$(data.name, '.block-' + i).style.backgroundColor = 'gray'
-  }
-}
--- index.js
+++ index.js
@@ -1,5 +1,119 @@
-var yo = require('yo-yo')
+var speedometer = require('speedometer')
+var prettyBytes = require('pretty-bytes')
 
-function Stats () {
+var $ = document.querySelector.bind(document)
 
+function Stats (el, interval) {
+  if (!(this instanceof Stats)) return new Stats(el, interval)
+  var self = this
+  self.feeds = {}
+  self.el = el
+
+  setInterval(function () {
+    var keys = Object.keys(self.feeds)
+    for (var i = 0; i < keys.length; i++) {
+      var st = self.feeds[keys[i]]
+      self.$$(keys[i], '.upload-speed').innerText = prettyBytes(st.uploadSpeed()) + '/s'
+      self.$$(keys[i], '.download-speed').innerText = prettyBytes(st.downloadSpeed()) + '/s'
+    }
+  }, interval || 500)
+}
+
+Stats.prototype.$$ = function (name, sel) {
+  return this.el.querySelector('#' + (name || 'unknown') + ' ' + sel)
+}
+
+Stats.prototype._get = function (name) {
+  var self = this
+  if (!name) name = 'unknown'
+  var st = self.feeds[name]
+  if (!st) {
+    var div = document.createElement('div')
+    div.id = name
+    div.innerHTML = self.el.querySelector('#template').innerHTML
+    div.className = 'feed'
+
+    if (name !== 'unknown') {
+      div.querySelector('.name').innerText = name
+    }
+
+    self.el.appendChild(div)
+
+    st = self.feeds[name] = {
+      blocks: 0,
+      uploadSpeed: speedometer(),
+      downloadSpeed: speedometer(),
+      div: div
+    }
+  }
+
+  return st
+}
+
+Stats.prototype._update = function () {
+  var self = this
+  var keys = Object.keys(self.feeds)
+  for (var i = 0; i < keys.length; i++) {
+    var st = self.feeds[keys[i]]
+    self.$$(keys[i], '.upload-speed').innerText = prettyBytes(st.uploadSpeed()) + '/s'
+    self.$$(keys[i], '.download-speed').innerText = prettyBytes(st.downloadSpeed()) + '/s'
+  }
+}
+
+Stats.prototype.onkey = function (data) {
+  var self = this
+  $('#key').innerText = data.key
+
+  while ($('.feed')) self.el.removeChild($('.feed'))
+  self.feeds = {}
+}
+
+function updateHeader (data) {
+  this.$$(data.name, '.overview').innerText = data.blocks.length + ' blocks (' + prettyBytes(data.bytes) + ')'
+}
+
+Stats.prototype.onpeerupdate = function (data) {
+  $('#peers').innerText = 'Connected to ' + data.peers + ' peer' + (data.peers === 1 ? '' : 's')
+}
+
+Stats.prototype.ondownload = function (data) {
+  this._get(data.name).downloadSpeed(data.bytes)
+  this.$$(data.name, '.block-' + data.index).style.backgroundColor = 'gray'
+}
+
+Stats.prototype.onupload = function (data) {
+  var self = this
+  this._get(data.name).uploadSpeed(data.bytes)
+  this.$$(data.name, '.block-' + data.index).style.backgroundColor = '#35b44f'
+  setTimeout(function () {
+    self.$$(data.name, '.block-' + data.index).style.backgroundColor = 'gray'
+  }, 500)
+}
+
+Stats.prototype.onfeed = function (data) {
+  var self = this
+  var blocks = this._get(data.name).blocks = data.blocks.length
+  updateHeader(data)
+
+  for (var i = 0; i < blocks; i++) {
+    self._appendDot(data.name, i)
+    if (data.blocks[i]) self.$$(data.name, '.block-' + i).style.backgroundColor = 'gray'
+  }
+}
+
+Stats.prototype.onupdate = function (data) {
+  var self = this
+  updateHeader(data)
+
+  for (var i = self._get(data.name).blocks; i < data.blocks.length; i++) {
+    self._get(data.name).blocks++
+    self._appendDot(data.name, i)
+    if (data.blocks[i]) self.$$(data.name, '.block-' + i).style.backgroundColor = 'gray'
+  }
+}
+
+Stats.prototype._appendDot = function (name, i) {
+  var div = document.createElement('div')
+  div.className = 'dot block-' + i
+  this.$$(name, '.blocks').appendChild(div)
 }
